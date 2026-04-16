8bdd03fb854f28dbbc5fb726f7954e10a6ee2c1b
NTC <2787322345@qq.com>
2026-04-16 09:52:37 +0800

    项目创建

---
+++ src/main/java/com/whxy/campusbooktrade2/CampusBookTrade2Application.java
@@ -0,0 +1,13 @@
+package com.whxy.campusbooktrade2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusBookTrade2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusBookTrade2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/whxy/campusbooktrade2/CampusBookTrade2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whxy.campusbooktrade2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusBookTrade2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
